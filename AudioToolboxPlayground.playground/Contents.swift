060cd9101c489f8e795cac5e467b5c526fcc3f43
Mike Muszynski <mmuszynski@gmail.com>
2015-12-18 14:49:57 -0500

    fft seems to work

--- AudioToolboxPlayground.playground/Contents.swift
+++ AudioToolboxPlayground.playground/Contents.swift
@@ -7,9 +7,11 @@ import Accelerate
 let url = [#FileReference(fileReferenceLiteral: "baah.wav")#]
 let violinUrl = [#FileReference(fileReferenceLiteral: "violin.wav")#]
 let donkey = [#FileReference(fileReferenceLiteral: "donkey.mp3")#]
+let tone100hz = [#FileReference(fileReferenceLiteral: "100hz44100.wav")#]
+
 //allocate the audio file ref and open it with the sheep URL
 var af = ExtAudioFileRef()
-var err: OSStatus = ExtAudioFileOpenURL(donkey as CFURL, &af)
+var err: OSStatus = ExtAudioFileOpenURL(tone100hz as CFURL, &af)
 guard err == noErr else {
     fatalError("unable to open extAudioFile: \(err)")
 }
@@ -23,6 +25,8 @@ err = ExtAudioFileGetProperty(af, kExtAudioFileProperty_FileDataFormat, &size, &
 guard err == noErr else {
     fatalError("unable to get file data format: \(err)")
 }
+
+let sampleRate = fileASBD.mSampleRate
 
 var clientASBD = AudioStreamBasicDescription()
 clientASBD.mSampleRate = fileASBD.mSampleRate
@@ -48,10 +52,10 @@ guard err == noErr else {
     fatalError("unable to get number of frames expected: \(err)")
 }
 
+let length = Double(numberOfFrames) / sampleRate
+
 //initialize a buffer and a place to put the final data
 let bufferFrames = 4096
-//var finalData = [Float]()
-
 let finalData = UnsafeMutablePointer<Float>(malloc(Int(numberOfFrames) * sizeof(Float.self)))
 
 //pack all this into a buffer list
@@ -93,18 +97,30 @@ guard err == noErr else {
 }
 
 //fft operations
-let frames = 4096
-let length = vDSP_Length(log2(CDouble(frames)))
-let setup = vDSP_create_fftsetup(length, FFTRadix(kFFTRadix2));
+let frames = Int(numberOfFrames)
+//let fft_length = vDSP_Length(log2(CDouble(frames)))
+let fft_length: UInt = 16
+let setup = vDSP_create_fftsetup(fft_length, FFTRadix(kFFTRadix2));
 
-let outReal = UnsafeMutablePointer<Float>(malloc(Int(numberOfFrames/2) * sizeof(Float.self)))
-let outImag = UnsafeMutablePointer<Float>(malloc(Int(numberOfFrames/2) * sizeof(Float.self)))
+let outReal = UnsafeMutablePointer<Float>(malloc(Int(frames/2) * sizeof(Float.self)))
+let outImag = UnsafeMutablePointer<Float>(malloc(Int(frames/2) * sizeof(Float.self)))
 
 var out = COMPLEX_SPLIT(realp: outReal, imagp: outImag)
 var dataAsComplex = UnsafePointer<COMPLEX>(finalData)
 
 vDSP_ctoz(dataAsComplex, 2, &out, 1, UInt(frames/2))
-vDSP_fft_zip(setup, &out, 1, length, Int32(FFT_FORWARD))
+vDSP_fft_zip(setup, &out, 1, fft_length, Int32(FFT_FORWARD))
+
+let binSize = sampleRate / Double(frames)
+let binFor1000 = 1000 / binSize
+
+let power = UnsafeMutablePointer<Float>(malloc(Int(frames/2) * sizeof(Float.self)))
+var highestPower:Float = 0
+var x = 0
+
+for i in 0..<frames/2 {
+    power[i] = outReal[i] * outReal[i] + outImag[i] * outImag[i]
+}
 
 free(finalData)
 free(outReal)
